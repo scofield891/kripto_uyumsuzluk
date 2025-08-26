000fdd5b8b81732b8ac4d052d7a5e10ac11c4b22
scofield891 <onurkircas@gmail.com>
2025-08-26 15:57:01 +0300

    Fix logging.handlers import

--- bot.py
+++ bot.py
@@ -38,7 +38,8 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
-file_handler = logging.handlers.RotatingFileHandler('bot.log', maxBytes=10*1024*1024, backupCount=5)
+from logging.handlers import RotatingFileHandler
+file_handler = RotatingFileHandler('bot.log', maxBytes=10*1024*1024, backupCount=5)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
